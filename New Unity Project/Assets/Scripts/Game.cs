f597fbe48895567706f117b71cb582b7857eb67d
Unknown <w.ciemniecki@gmail.com>
2018-10-11 12:25:02 +0200

    Created Scheduler and .gitignore update

---
+++ New Unity Project/Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Game : MonoBehaviour
+    {
+        [SerializeField] float delay;
+
+        Action action = () => { Debug.Log("TEST"); };
+
+        void Start()
+        {
+            Scheduler.Instance.Schedule(delay, action);
+        }
+    }
+}
